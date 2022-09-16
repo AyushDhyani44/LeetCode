7fcf6be3d1657dabca982180b93ec5bea6be1993
AyushDhyani44 <100957206+AyushDhyani44@users.noreply.github.com>
2022-09-16 21:56:33 +0530

    Time: 6 ms (25.64%), Space: 6 MB (25.88%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -5,18 +5,18 @@ public:
          int i,sum,digit;
          while(num>9)
           {  sum=0;
-//                 for(i=num;i>0;i=i/10)
-//             {
-//               digit=i%10;
-//               sum=sum + digit;
+                for(i=num;i>0;i=i/10)
+            {
+              digit=i%10;
+              sum=sum + digit;
              
-//             }
-             while(num>0)
-             {
-                 int digit=num%10;
-                 sum=sum+digit;
-                 num=num/10;
-             }
+            }
+             // while(num>0)
+             // {
+             //     int digit=num%10;
+             //     sum=sum+digit;
+             //     num=num/10;
+             // }
             num=sum;
     }
         return num;
